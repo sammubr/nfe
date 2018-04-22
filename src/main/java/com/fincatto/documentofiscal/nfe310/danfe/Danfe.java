7b06bda12026c3bc1f25e3ef383aa98e3d25dc92
Samuel Noswitz <sammubr@gmail.com>
2018-04-22 14:14:17 -0300

    Geração de danfe para NF-e

--- src/main/java/com/fincatto/documentofiscal/nfe310/danfe/Danfe.java
+++ src/main/java/com/fincatto/documentofiscal/nfe310/danfe/Danfe.java
@@ -3,6 +3,7 @@ package com.fincatto.documentofiscal.nfe310.danfe;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
@@ -18,6 +19,7 @@ import com.fincatto.documentofiscal.DFModelo;
 import com.fincatto.documentofiscal.nfe310.classes.nota.NFNotaProcessada;
 import com.fincatto.documentofiscal.parsers.DFParser;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRXmlDataSource;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.zxing.BarcodeFormat;
@@ -37,14 +39,18 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class Danfe {
 
-    private final NFNotaProcessada nota;
+    private NFNotaProcessada notaProcessada;
 
     public Danfe(String xml) {
         this(new DFParser().notaProcessadaParaObjeto(xml));
     }
 
-    public Danfe(NFNotaProcessada nota) {
-        this.nota = nota;
+    public Danfe(NFNotaProcessada notaProcessada) {
+        this.notaProcessada = notaProcessada;
+    }
+
+    public byte[] gerarDanfeNFe(byte[] logoEmpresa) throws Exception {
+        return toPDF(createJasperPrintNFe(logoEmpresa));
     }
 
     public byte[] gerarDanfeNFCe(String informacoesComplementares, boolean mostrarMsgFinalizacao, NFCePagamento... pags) throws Exception {
@@ -55,16 +61,39 @@ public class Danfe {
         return JasperExportManager.exportReportToPdf(print);
     }
 
+    private JasperPrint createJasperPrintNFe(byte[] logoEmpresa) throws IOException, JRException, ParserConfigurationException, SAXException {
+        if (!DFModelo.NFE.equals(notaProcessada.getNota().getInfo().getIdentificacao().getModelo())) {
+            throw new IllegalStateException("Nao e possivel gerar DANFe NFe de uma NFCe");
+        }
+
+        try (InputStream in = Danfe.class.getClassLoader().getResourceAsStream("danfe/DANFE_NFE_RETRATO.jrxml");
+             InputStream subDuplicatas = Danfe.class.getClassLoader().getResourceAsStream("danfe/DANFE_NFE_DUPLICATAS.jrxml")) {
+
+            final JRPropertiesUtil jrPropertiesUtil = JRPropertiesUtil.getInstance(DefaultJasperReportsContext.getInstance());
+            jrPropertiesUtil.setProperty("net.sf.jasperreports.xpath.executer.factory", "net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
+
+
+            Map<String, Object> parameters = new HashMap<>();
+            JasperReport subDuplicatasComp = JasperCompileManager.compileReport(subDuplicatas);
+            parameters.put("SUBREPORT_DUPLICATAS", subDuplicatasComp);
+            parameters.put("LOGO_EMPRESA", (logoEmpresa == null ? null : new ByteArrayInputStream(logoEmpresa)));
+
+            JasperReport report = JasperCompileManager.compileReport(in);
+            return JasperFillManager.fillReport(report, parameters, new JRXmlDataSource(convertStringXMl2DOM(), "/nfeProc/NFe/infNFe/det"));
+        }
+
+    }
+
     private Document convertStringXMl2DOM() throws ParserConfigurationException, IOException, SAXException {
-        try (StringReader stringReader = new StringReader(nota.toString())) {
+        try (StringReader stringReader = new StringReader(notaProcessada.toString())) {
             InputSource inputSource = new InputSource();
             inputSource.setCharacterStream(stringReader);
             return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource);
         }
     }
 
-    public JasperPrint createJasperPrintNFCe(String informacoesComplementares, boolean mostrarMsgFinalizacao, NFCePagamento... pags) throws IOException, WriterException, JRException {
-        if (!DFModelo.NFCE.equals(nota.getNota().getInfo().getIdentificacao().getModelo())) {
+    private JasperPrint createJasperPrintNFCe(String informacoesComplementares, boolean mostrarMsgFinalizacao, NFCePagamento... pags) throws IOException, WriterException, JRException {
+        if (!DFModelo.NFCE.equals(notaProcessada.getNota().getInfo().getIdentificacao().getModelo())) {
             throw new IllegalStateException("Nao e possivel gerar DANFe NFCe de uma NFe");
         }
 
@@ -72,7 +101,7 @@ public class Danfe {
              InputStream subItens = Danfe.class.getClassLoader().getResourceAsStream("danfe/DANFE_NFCE_ITENS.jrxml");
              InputStream subPagamentos = Danfe.class.getClassLoader().getResourceAsStream("danfe/DANFE_NFCE_PAGAMENTOS.jrxml")) {
 
-            boolean homologacao = nota.getNota().getInfo().getIdentificacao().getAmbiente().equals(DFAmbiente.HOMOLOGACAO);
+            boolean homologacao = notaProcessada.getNota().getInfo().getIdentificacao().getAmbiente().equals(DFAmbiente.HOMOLOGACAO);
             List<NFCePagamento> pgtos = Arrays.asList(pags);
 
             Map<String, Object> parameters = new HashMap<>();
@@ -85,23 +114,22 @@ public class Danfe {
             parameters.put("CHAVE_ACESSO_FORMATADA", formatarChaveAcesso());
             parameters.put("INFORMACOES_COMPLEMENTARES", informacoesComplementares);
             parameters.put("MOSTRAR_MSG_FINALIZACAO", mostrarMsgFinalizacao);
-            parameters.put("URL_CONSULTA", homologacao ? nota.getNota().getInfo().getIdentificacao().getUf().getQrCodeHomologacao() :
-                    nota.getNota().getInfo().getIdentificacao().getUf().getQrCodeProducao());
+            parameters.put("URL_CONSULTA", homologacao ? notaProcessada.getNota().getInfo().getIdentificacao().getUf().getQrCodeHomologacao() : notaProcessada.getNota().getInfo().getIdentificacao().getUf().getQrCodeProducao());
 
             JasperReport report = JasperCompileManager.compileReport(in);
-            return JasperFillManager.fillReport(report, parameters, new JRBeanArrayDataSource(new Object[]{nota}));
+            return JasperFillManager.fillReport(report, parameters, new JRBeanArrayDataSource(new Object[] { notaProcessada }));
         }
     }
 
     private String formatarChaveAcesso() {
-        return StringUtils.join(nota.getNota().getInfo().getChaveAcesso().split("(?<=\\G....)"), " ");
+        return StringUtils.join(notaProcessada.getNota().getInfo().getChaveAcesso().split("(?<=\\G....)"), " ");
     }
 
     /**
      * Geracao do QRCode com ZXing
      * http://repo1.maven.org/maven2/com/google/zxing/core/3.2.0/
      */
-    public BufferedImage gerarQRCode() throws WriterException {
+    private BufferedImage gerarQRCode() throws WriterException {
         int size = 250;
         Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
         hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
@@ -109,8 +137,7 @@ public class Danfe {
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix byteMatrix = qrCodeWriter.encode(nota.getNota().getInfoSuplementar().getQrCode(),
-                BarcodeFormat.QR_CODE, size, size, hintMap);
+        BitMatrix byteMatrix = qrCodeWriter.encode(notaProcessada.getNota().getInfoSuplementar().getQrCode(), BarcodeFormat.QR_CODE, size, size, hintMap);
         int crunchifyWidth = byteMatrix.getWidth();
         BufferedImage image = new BufferedImage(crunchifyWidth, crunchifyWidth, BufferedImage.TYPE_INT_RGB);
         image.createGraphics();
